5eb8b0ec14db9a99cc9c07606c8c131194a7840e
Leeroy Chako <leeroychako@gmail.com>
2026-01-24 16:42:53 +0200

    Updated libraries and add the Leva and r3f-perf monitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Perf } from "r3f-perf";
+import { Leva } from "leva";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Leva collapsed={true} />
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Perf position="top-left" />
+        <Experience />
+      </Canvas>
+    </>
   );
 }
 
